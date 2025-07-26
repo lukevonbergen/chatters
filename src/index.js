12bc697f66166bab43e65b142948b11859003e27
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-07-26 15:16:20 +0000

    sessioncontextprovider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-react';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabaseClient = createBrowserSupabaseClient();
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <App />
+    </SessionContextProvider>
+  </BrowserRouter>
+);
