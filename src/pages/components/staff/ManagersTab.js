4a2188235f3feb72be20595d182e009ab9a1d4fc
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-07-30 14:15:24 +0000

    staff

--- src/pages/components/staff/ManagersTab.js
+++ src/pages/components/staff/ManagersTab.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../../utils/supabase';
+import { supabase } from '../../../utils/supabase';
 
 const ManagersTab = ({ 
   managers, 
